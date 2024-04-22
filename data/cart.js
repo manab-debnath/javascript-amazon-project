5e4dc79cba9018e46f22fd9e51e385834043facb
manab-debnath <manab_debnath@outlook.com>
2024-04-22 22:32:04 +0530

    move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+
+export function addToCart(productId)    {
+    let matchingItem;
+    cart.forEach((CartItem) => {
+        if(productId === CartItem.productId)
+            matchingItem = CartItem;
+    });
+
+    if(matchingItem)    matchingItem.quantity++;
+    else    {    
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+}
