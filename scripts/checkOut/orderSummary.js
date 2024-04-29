1d65f212953c31fd72371826d73a1c2b325b2138
manab-debnath <manab_debnath@outlook.com>
2024-04-30 00:42:03 +0530

    Make payment summary interactive

--- scripts/checkOut/orderSummary.js
+++ scripts/checkOut/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOption.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 
 
@@ -132,6 +133,8 @@ export function renderOrderSummary()   {
             );  
 
             container.remove();
+
+            renderPaymentSummary();
         });
     });
 
@@ -141,6 +144,8 @@ export function renderOrderSummary()   {
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+
+            renderPaymentSummary();
         });
     });
 
