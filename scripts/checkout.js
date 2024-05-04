a3f5aacbfd7afadad5503edebff309c1bc73d454
manab-debnath <manab_debnath@outlook.com>
2024-05-04 23:58:04 +0530

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 
 
