9035755b9da38020e24eec935ba43b1769178270
manab-debnath <manab_debnath@outlook.com>
2024-05-06 00:17:33 +0530

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -60,6 +60,34 @@ class clothing extends Product  {
 }
 
 
+/* const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString()); */
+
+/* console.log(this);    // we can use 'this' method anywhere in javascript
+const object2 = {
+  a: 2,
+  b: this.a     // in this case a does not created yet so it show 'undefined'
+}; */
+
+/* 
+function logThis()  {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+
+this;
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method(); */
+
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
