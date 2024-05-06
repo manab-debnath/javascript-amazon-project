e7cf667ba3b90a4f2e8e8760513d13f66fd177d5
manab-debnath <manab_debnath@outlook.com>
2024-05-06 22:24:02 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
 // import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 
 
