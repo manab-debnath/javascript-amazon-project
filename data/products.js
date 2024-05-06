6abe653c286f4d7d7cd92e198d64e643916d754d
manab-debnath <manab_debnath@outlook.com>
2024-05-07 00:38:20 +0530

    Load Products from the backend

--- data/products.js
+++ data/products.js
@@ -87,7 +87,29 @@ const object3 = {
 object3.method(); */
 
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing')  {
+        return  new clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+// loadProducts();
+
+/* 
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -755,3 +777,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+ */
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,13 @@
 import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
 
 
 
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+      renderOrderSummary();
+      renderPaymentSummary();
+});
