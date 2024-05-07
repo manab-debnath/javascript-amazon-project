5cb57109a392738e2115385c58fa9c2e851fdbb0
manab-debnath <manab_debnath@outlook.com>
2024-05-07 22:17:38 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,11 +3,54 @@ import { renderPaymentSummary } from './checkOut/paymentSummary.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
 
 
+Promise.all([
+      new Promise((resolve) => {
+            loadProducts(() => {
+                  resolve('value1');
+            });
+      }),
+      new Promise((resolve) => {
+            loadCart(() => {
+                  resolve();
+            });            
+      })
 
-loadProducts(() => {
+]).then((value) => {
+      console.log(value);
+      
       renderOrderSummary();
       renderPaymentSummary();
 });
+
+
+
+/* new Promise((resolve) => {
+      loadProducts(() => {
+            resolve('value1');
+      });
+}).then((value) => {
+      console.log(value);
+
+      return new Promise((resolve) => {
+            loadCart(() => {
+                  resolve();
+            });            
+      });
+}).then(() => {
+      renderOrderSummary();
+      renderPaymentSummary();
+}); */
+
+
+
+
+/* loadProducts(() => {
+      loadCart(() => {
+            renderOrderSummary();
+            renderPaymentSummary();
+      });
+}); */
