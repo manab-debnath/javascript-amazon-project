ea6a85db5437107fb16aaca181bf444f56205a61
manab-debnath <manab_debnath@outlook.com>
2024-05-08 20:58:28 +0530

    Convert from promises to async wait

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,6 +7,24 @@ import { loadCart } from '../data/cart.js';
 
 
 
+async function loadPage()     {
+
+      await loadProductsFetch();
+
+      await new Promise((resolve) => {
+            loadCart(() => {
+                  resolve();
+            });            
+      });
+      
+      renderOrderSummary();
+      renderPaymentSummary();
+
+}
+
+loadPage();
+
+/* 
 Promise.all([
       loadProductsFetch(),
       new Promise((resolve) => {
@@ -21,7 +39,7 @@ Promise.all([
       renderOrderSummary();
       renderPaymentSummary();
 });
-
+ */
 
 
 /* new Promise((resolve) => {
